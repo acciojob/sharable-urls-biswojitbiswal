4985edbe09a098c78783df02bce43170d6e47bcc
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-29 12:16:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // your code here
-// your code here
 let btn = document.getElementById("submit");
 let nameInput = document.querySelector("name");
 let yearInput = document.querySelector("year");
@@ -8,5 +7,20 @@ let url = document.getElementById("heading");
 url.textContent = "https://localhost:8080/";
 
 btn.addEventListener("click", () => {
-	url.textContent = `https://localhost:8080/?name=${nameInput.value ? nameInput.value : ""}&year=${yearInput.value ? yearInput : ""}`;
+	const name = nameInput.value ? nameInput.value : "";
+    const year = yearInput.value ? yearInput.value : "";
+
+    let baseUrl = `https://localhost:8080/?`;
+
+    let queryParams = [];
+
+    if (name) {
+        queryParams.push(`name=${name}`);
+    }
+
+    if (year) {
+        queryParams.push(`year=${year}`);
+    }
+
+    url.textContent = baseUrl + queryParams.join('&');
 })
