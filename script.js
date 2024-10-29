1836fedfc90a78217834c36c4f6b5ee5cd630e64
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-29 11:55:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // your code here
+// your code here
+let btn = document.getElementById("submit");
+let nameInput = document.querySelector("input[name='name']");
+let yearInput = document.querySelector("input[name='year']");
+let url = document.getElementById("heading");
+
+url.textContent = "https://localhost:8080/";
+
+btn.addEventListener("click", () => {
+	url.textContent = `https://localhost:8080/?name=${nameInput.value ? nameInput.value : ""}&year=${yearInput.value ? yearInput : ""}`;
+})
